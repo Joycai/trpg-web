acc0011462bd6fe1968f09bcd24feb28a35e3478
zhengxucai <85534078@qq.com>
2019-07-22 16:37:20 +0800

    -re init

--- trpg-web/src/views/Home.vue
+++ trpg-web/src/views/Home.vue
@@ -1,46 +1,18 @@
 <template>
-  <div id="main">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/trpg/home">TestPage</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <div class="home">
-      <img alt="Vue logo" src="../assets/logo.png">
-      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    </div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<style lang="scss">
-#main {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
-
-<script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default Vue.extend({
-  name: "home",
+export default {
+  name: 'home',
   components: {
     HelloWorld
   }
-});
+}
 </script>
