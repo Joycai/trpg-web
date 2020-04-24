ef5d9f25b27b0dff0a192bd3a3eaa80bd3d6a9e5
Joycai <85534078@qq.com>
2020-04-24 16:25:42 +0800

    -update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button @click="handleClick ">about</a-button>
   </div>
 </template>
+
+<script>
+export default {
+  methods:{
+    handleClick: function(){
+      this.$router.push("/about")
+    }
+  }
+}
+</script>
